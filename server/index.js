9f32a56fef2581a711e2f4f885bcd66afd15c2b3
truepatch <truepatch@gmail.com>
2022-03-15 01:03:12 +0500

    add socket.io

--- server/index.js
+++ server/index.js
@@ -1,11 +1,43 @@
-const express = require('express')
-require('dotenv').config()
+const app = require('express')();
+const http = require('http').createServer(app);
+const io = require('socket.io')(http);
+require('dotenv').config();
 
+//Читаем переменные из среды исполнения
 const host = process.env.SERVER_HOST
 const port = process.env.SERVER_PORT
-const app = express()
 
+app.get('/', (req, res) => {
+  res.send('<h1>Hello world</h1>');
+});
 
-app.listen(port, host, () =>
-  console.log(`Server listens http://${host}:${port}`)
-)
+//Выпролняется каждый раз при новом подключении
+io.on('connection', function(socket) {
+    console.log('A user Connected');
+
+   //Подписываемся на канал события отправленного сообщения
+   socket.on('chat_message', (data) => {
+        //тут же отправляем всем клиентам полученные данные
+        io.emit('chat_message', {
+            name: data.name,
+            message: data.message,
+        })
+   });
+   //Слушаем канал 
+   socket.on('user_status', (data) => {
+        //Отправляем всем клиентам полученные данные
+        io.emit('chat_message', {
+            name: data.name,
+            status: data.status,
+        })
+   });
+
+   //Выполняется каждый раз при отключении
+   socket.on('disconnect', function () {
+      console.log('A user Disconnected');
+   });
+});
+
+http.listen(port, function() {
+   console.log(`Server listens http://${host}:${port}`)
+});
